ef51a68225e7623557207ce7146b61b7fdcb9eb3
abhinmamit <abhinmamit@gmail.com>
2016-06-06 18:55:48 +0530

    4th commit

--- myProject/src/packg/JavaClass.java
+++ myProject/src/packg/JavaClass.java
@@ -3,7 +3,7 @@ package packg;
 public class JavaClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Java World 1");
+		System.out.println("Hello Java World 12");
 
 	}
 
